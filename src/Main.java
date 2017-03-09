d7b902b0e04f6a1510d40f95507ad4d308dbd084
ikaslea <ikaslea@gela07_02>
2017-03-09 13:06:02 +0100

    comprobar platos konpontzeko

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -9,10 +10,15 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import javax.swing.ButtonGroup;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -41,10 +47,13 @@ public class Main extends JFrame {
 		setContentPane(contentPane);
 		
 		JRadioButton radioTortillaConCebolla = new JRadioButton("Tortilla con cebolla");
+		buttonGroup.add(radioTortillaConCebolla);
 		
 		JRadioButton radioTortillaSinCebolla = new JRadioButton("Tortilla sin cebolla");
+		buttonGroup.add(radioTortillaSinCebolla);
 		
 		JRadioButton radioTortillaFrancesaCon = new JRadioButton("Tortilla francesa con jam\u00F3n");
+		buttonGroup.add(radioTortillaFrancesaCon);
 		
 		JCheckBox checkPatata = new JCheckBox("Patata");
 		
@@ -61,37 +70,76 @@ public class Main extends JFrame {
 		JCheckBox checkPimienta = new JCheckBox("Pimienta");
 		
 		JCheckBox checkLetxugie = new JCheckBox("Letxugie");
+		
+		JButton buttonComprobar = new JButton("Comprobar");
+		buttonComprobar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(radioTortillaConCebolla.isSelected()){ 
+					if(checkPatata.isSelected() && checkArrotzie.isSelected() && checkCebollie.isSelected() && !checkJamoie.isSelected() && !checkTxorizue.isSelected() && !checkPiperra.isSelected() && !checkPimienta.isSelected() && !checkLetxugie.isSelected()){
+						buttonComprobar.setBackground(new Color(168, 199, 100));
+					}else{
+						buttonComprobar.setBackground(new Color(128, 24, 21));
+						checkPatata.setSelected(false);
+						checkArrotzie.setSelected(false);
+						checkCebollie.setSelected(false);
+						
+					}
+				}else if(radioTortillaSinCebolla.isSelected()){
+					if(checkPatata.isSelected() && checkArrotzie.isSelected() && !checkCebollie.isSelected() && !checkJamoie.isSelected() && !checkTxorizue.isSelected() && !checkPiperra.isSelected() && !checkPimienta.isSelected() && !checkLetxugie.isSelected()){
+						buttonComprobar.setBackground(new Color(168, 199, 100));
+					}else{
+						buttonComprobar.setBackground(new Color(128, 24, 21));
+						checkPatata.setSelected(false);
+						checkArrotzie.setEnabled(false);
+					}
+				}else if(radioTortillaFrancesaCon.isSelected()){
+					if(!checkPatata.isSelected() && checkArrotzie.isSelected() && !checkCebollie.isSelected() && checkJamoie.isSelected() && !checkTxorizue.isSelected() && !checkPiperra.isSelected() && !checkPimienta.isSelected() && !checkLetxugie.isSelected()){
+						buttonComprobar.setBackground(new Color(168, 199, 100));
+					}else{
+						buttonComprobar.setBackground(new Color(128, 24, 21));
+						checkJamoie.setSelected(false);
+						checkArrotzie.setSelected(false);
+					}
+				}
+				
+			}
+		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(radioTortillaConCebolla)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(radioTortillaSinCebolla)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(radioTortillaFrancesaCon))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(checkPimienta)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(checkPatata)
-									.addGap(8)
-									.addComponent(checkArrotzie)))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(checkCebollie)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addContainerGap()
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(checkJamoie)
+									.addComponent(radioTortillaConCebolla)
 									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(checkTxorizue)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(checkPiperra))
-								.addComponent(checkLetxugie))))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+									.addComponent(radioTortillaSinCebolla)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(radioTortillaFrancesaCon))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addComponent(checkPimienta)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addComponent(checkPatata)
+											.addGap(8)
+											.addComponent(checkArrotzie)))
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(checkCebollie)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addComponent(checkJamoie)
+											.addPreferredGap(ComponentPlacement.UNRELATED)
+											.addComponent(checkTxorizue)
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addComponent(checkPiperra))
+										.addComponent(checkLetxugie)))))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(134)
+							.addComponent(buttonComprobar)))
+					.addContainerGap(36, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -113,7 +161,9 @@ public class Main extends JFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(checkPimienta)
 						.addComponent(checkLetxugie))
-					.addContainerGap(86, Short.MAX_VALUE))
+					.addGap(32)
+					.addComponent(buttonComprobar)
+					.addContainerGap(31, Short.MAX_VALUE))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
